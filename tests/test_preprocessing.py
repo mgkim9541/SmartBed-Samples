7644f3b635b0717358be7e831444f46df4815d65
mgkim9541 <108269156+mgkim9541@users.noreply.github.com>
2025-04-30 14:41:18 +0900

    Delete tests directory

--- tests/test_preprocessing.py
+++
@@ -1,7 +0,0 @@
-from Codes.preprocessing import preprocess
-import pandas as pd
-
-def test_preprocess():
-    df = pd.DataFrame({'Com':[0,1], 'A':[1,2]})
-    X_train, X_test, y_train, y_test = preprocess(df, target='Com')
-    assert X_train.shape[0] == 1
